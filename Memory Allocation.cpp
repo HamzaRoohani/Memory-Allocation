4cf9709b88313c69b80a6f83fe22ea2c8c89d398
HamzaRoohani <114760877+HamzaRoohani@users.noreply.github.com>
2022-09-30 22:37:08 +0500

    Add files via upload

---
+++ Memory Allocation.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int var1,var2,var3;
+	var1=10;
+	var2=20;
+	var3=30;
+	cout<<"The Memory Location of the variable 1 = "<<var1<<" is:"<<&var1<<endl;
+	cout<<"The Memory Location of the variable 2 = "<<var2<<" is:"<<&var2<<endl;
+	cout<<"The Memory Location of the variable 3 = "<<var3<<" is:"<<&var3<<endl;
+}
